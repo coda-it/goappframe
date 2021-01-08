e42e70bdd17d84c85e294b78d354e69470a76811
Oskar Szura <oskar.szura@gmail.com>
2021-01-08 08:58:13 +0100

    CODA-133 - Application framework

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # goappframe
 Go based application framework
+
+### Motivation
+A lot of application logic is duplicated across multiple projects. This framework aim to gather it all and be re-used across projects.
--- app/internals.go
+++ app/internals.go
@@ -1,8 +1,8 @@
 package app
 
 import (
-	"github.com/coda-it/goutils/mailer"
 	"github.com/coda-it/goappframe/module"
+	"github.com/coda-it/goutils/mailer"
 	"github.com/coda-it/gowebserver/router"
 )
 
@@ -11,7 +11,7 @@ type Internals struct {
 	Port        string
 	Modules     []module.Module
 	Persistence IPersistance
-	DataKey		string
+	DataKey     string
 	Mailer      mailer.IMailer
 	NotFound    router.ControllerHandler
 }
