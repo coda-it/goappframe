065672e231659eef971e7b32ab5bae564da27647
Oskar Szura <oskarszura@Air-Oskar.lan>
2024-04-17 07:52:43 +0200

    CODA-294 - Navigation should be defined per application

--- config/apps.go
+++ config/apps.go
@@ -1,8 +1,11 @@
 package config
 
+import "github.com/coda-it/goappframe/navigation"
+
 // App - application config
 type App struct {
-	ID      string   `json:"id"`
-	Modules []Module `json:"modules"`
-	Domain  string   `json:"domain"`
+	ID         string                  `json:"id"`
+	Modules    []Module                `json:"modules"`
+	Domain     string                  `json:"domain"`
+	Navigation []navigation.Navigation `json:"navigation"`
 }
--- config/config.go
+++ config/config.go
@@ -1,14 +1,9 @@
 package config
 
-import (
-	"github.com/coda-it/goappframe/navigation"
-)
-
 // Config - static app config
 type Config struct {
-	Navigation      []navigation.Navigation `json:"navigation"`
-	Apps            []App                   `json:"apps"`
-	DefaultLanguage string                  `json:"defaultLanguage"`
-	Cert            string                  `json:"certificate"`
-	CertPrvKey      string                  `json:"certificatePrivateKey"`
+	Apps            []App  `json:"apps"`
+	DefaultLanguage string `json:"defaultLanguage"`
+	Cert            string `json:"certificate"`
+	CertPrvKey      string `json:"certificatePrivateKey"`
 }
