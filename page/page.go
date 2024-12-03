d1ad4bbdc6766123685d21e16959ca802ec40cc0
Oskar Szura <oskar.szura@gmail.com>
2024-12-03 12:50:41 +0100

    CODA-352 - Feature flags

--- page/page.go
+++ page/page.go
@@ -4,14 +4,16 @@ import "github.com/coda-it/goappframe/navigation"
 
 // Page - entity representing page
 type Page struct {
-	Version        string
-	Title          string
-	IsLogged       bool
-	IsRoot         bool
-	Params         map[string]interface{}
-	Name           string
-	Navigation     []navigation.Navigation
-	JSConfig       string
-	Translations   map[string]string
-	JSTranslations string
+	Version         string
+	Title           string
+	IsLogged        bool
+	IsRoot          bool
+	Params          map[string]interface{}
+	Name            string
+	Navigation      []navigation.Navigation
+	JSConfig        string
+	Translations    map[string]string
+	JSTranslations  string
+	FeatureFlags    map[string]bool
+	JSFeaturesFlags string
 }
